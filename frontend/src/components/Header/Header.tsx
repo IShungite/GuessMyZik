36460057a8a6b003832e39d79a27266f7c491e9f
IShungite <56173955+IShungite@users.noreply.github.com>
2022-11-04 17:47:19 +0100

    add react router & remove default react-vite screen

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <div>
+      <ul>
+        <li className="space-x-2 underline">
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
