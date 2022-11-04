9f0b65e38a7e9c771fd08b157cc6e23b44647b0d
IShungite <56173955+IShungite@users.noreply.github.com>
2022-11-04 18:29:43 +0100

    add protected route

---
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+export default function ProtectedRoute() {
+  const isAuthenticated = false;
+
+  if (!isAuthenticated) {
+    return <Navigate to={"/login"} />;
+  }
+
+  return <Outlet />;
+}
