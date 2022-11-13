21545fc86b50fe7a5cb0f44e15749994eb674322
IShungite <56173955+IShungite@users.noreply.github.com>
2022-11-13 15:14:05 +0100

    feat: ✨ #13 - Add deezer service

---
+++ backend/src/api/tryFetch.ts
@@ -0,0 +1,10 @@
+export default async function tryFetch<T>(url: string, init?: RequestInit): Promise<T> {
+  try {
+    const response = await fetch(url, init);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error while fetching data");
+  }
+}
