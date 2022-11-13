abe5b928119ca99f8f81e7b1644b9c63cc1b5087
IShungite <56173955+IShungite@users.noreply.github.com>
2022-11-13 14:48:36 +0100

    add eslint to frontend

--- frontend/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 
 export default function ProtectedRoute() {
   const isAuthenticated = false;
 
   if (!isAuthenticated) {
-    return <Navigate to={"/login"} />;
+    return <Navigate to={'/login'} />;
   }
 
   return <Outlet />;
