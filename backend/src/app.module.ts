0bb31ed4faf73a5f09dc6c5d4c51b704d1e061bf
Adrrick <adrikgw2@gmail.com>
2022-11-17 11:45:54 +0100

    feat: :sparkles: Login Backend
    Added auth.controller and jwt token login logic

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
