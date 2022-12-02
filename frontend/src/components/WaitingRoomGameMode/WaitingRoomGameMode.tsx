bfcf0408430b30997a4d385b31fe30207549fdbc
IShungite <56173955+IShungite@users.noreply.github.com>
2022-12-02 19:20:57 +0100

    feat: ✨ #11 Add the possibility to change the game mode

--- frontend/src/components/WaitingRoomGameMode/WaitingRoomGameMode.tsx
+++ frontend/src/components/WaitingRoomGameMode/WaitingRoomGameMode.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
-import { useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
+import { GameMode } from '../../@Types/Game';
 import { gameModeAtom } from '../../atoms/gameAtom';
+import gameService from '../../services/gameService';
+import socketService from '../../services/socketService';
+
+function enumKeys<O extends object, K extends keyof O = keyof O>(obj: O): K[] {
+    return Object.keys(obj).filter(k => Number.isNaN(+k)) as K[];
+}
 
 export default function WaitingRoomGameMode() {
-    const gameMode = useRecoilValue(gameModeAtom);
+    const [gameMode, setGameMode] = useRecoilState(gameModeAtom);
+
+
+    const handleChangeGameMode = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const newGameMode = e.target.value as GameMode;
+
+        if (socketService.socket) {
+            gameService.updateGame(socketService.socket, { gameMode: newGameMode });
+            setGameMode(newGameMode);
+        }
+    }
 
     return (
-        <div>Game mode: {gameMode}</div>
+        <div className='flex'>
+            <div>Game mode:</div>
+            <select onChange={handleChangeGameMode} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                {enumKeys(GameMode).map((key) => (
+                    <option key={key} value={key} defaultValue={gameMode}>{key}</option>
+                ))}
+            </select>
+        </div>
     )
 }
