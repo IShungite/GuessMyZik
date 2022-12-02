79821b693fe3cdc2147d7379203f335c10936092
IShungite <56173955+IShungite@users.noreply.github.com>
2022-12-02 16:34:19 +0100

    Fix search playlist input

--- frontend/src/components/SearchPlaylistInput/SearchPlaylistInput.tsx
+++ frontend/src/components/SearchPlaylistInput/SearchPlaylistInput.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { useDebounce } from "usehooks-ts";
 import { deezerPlaylistsSearchTermState } from "../../atoms/deezerPlaylistsAtom";
 
@@ -7,7 +7,7 @@ export default function SearchPlaylistInput() {
   const [searchTerm, setSearchTerm] = useState("");
   const debouncedValue = useDebounce<string>(searchTerm, 500);
 
-  const setDeezerPlaylistsSearchTerm = useSetRecoilState(deezerPlaylistsSearchTermState);
+  const [deezerPlaylistsSearchTerm, setDeezerPlaylistsSearchTerm] = useRecoilState(deezerPlaylistsSearchTermState);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
@@ -17,5 +17,9 @@ export default function SearchPlaylistInput() {
     setDeezerPlaylistsSearchTerm(debouncedValue);
   }, [debouncedValue]);
 
-  return <input className="border border-cyan-800 rounded-md" onChange={handleInputChange} />;
+  useEffect(() => {
+    setSearchTerm(deezerPlaylistsSearchTerm);
+  }, [deezerPlaylistsSearchTerm]);
+
+  return <input className="border border-cyan-800 rounded-md" value={searchTerm} onChange={handleInputChange} />;
 }
