468296c06b2b485608d036a1c3bdc57a12f82491
IShungite <56173955+IShungite@users.noreply.github.com>
2022-12-08 17:26:08 +0100

    update join game title

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       </div >
 
       <div className='max-w-sm min-h-max p-6 bg-white border border-gray-200 rounded-lg shadow-md  dark:bg-gray-800 dark:border-gray-700'>
-        <div className="mb-2">Create a new game</div>
+        <div className="mb-2">Join a game</div>
         <JoinForm />
       </div>
     </div >
